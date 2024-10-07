37dc91ce6afecc2e740892441666e8f1dad95082
Abrunheiro, Carlos <carlos.abrunheiro@sicpa.com>
2024-10-07 15:40:12 +0100

    Add required privacy manifest file

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "Kingfisher",
+    platforms: [.iOS(.v12), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "Kingfisher", targets: ["Kingfisher"])
+    ],
+    targets: [
+        .target(
+            name: "Kingfisher",
+            path: "Sources",
+            resources: [.process("PrivacyInfo.xcprivacy")]
+        )
+    ]
+)
